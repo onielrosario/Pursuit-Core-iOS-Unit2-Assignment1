cf8e4543ad39eeb3bbe465efa0414bc6460ab465
onielrosario <onielrosario@pursuit.org>
2018-11-08 21:10:25 -0500

    first commit

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -9,5 +9,39 @@
 import Foundation
 
 class TicTacToeBrain {
-  
+    var playerTurn = Turn.playerOne
+    
+    
+    enum Turn: String {
+        case playerOne = "X"
+        case playerTwo = "O"
+        mutating func toggle() {
+            switch self {
+            case .playerOne: self = .playerTwo
+            case .playerTwo: self = .playerOne
+            }
+        }
+    }
+    
+    func updateBrain(x: Int, y: Int){
+        playerTurn.toggle()
+        let square: Square
+        switch playerTurn {
+        case .playerOne: square = .x
+        case .playerTwo: square = .o
+        }
+        
+    }
+    
+    enum Square {
+        case x
+        case o
+        case empty
+        
+    }
+    
+    var gameboard =  [[Square]](repeating: [Square](repeating: .empty, count: 3), count: 3)
+    
+    
+    
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    var gameBrain: TicTacToeBrain = TicTacToeBrain()
+    
+    
+    
+    
+    
+    
+    
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-  }
+    }
+    
+    @IBAction func buttonPressed(_ sender: GameButton) {
+        let turn = gameBrain.playerTurn
+        
+        sender.setTitle(turn.rawValue, for: .normal)
+        
+        gameBrain.updateBrain(x: sender.row, y: sender.col)
+        
+        print(sender)
+       
+        sender.isEnabled = false
+        ///sender.backgroundColor = .black
+    }
+    
+    
 
 
 }
