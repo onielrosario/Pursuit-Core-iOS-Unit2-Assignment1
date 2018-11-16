fc0d33ccdd9534f76559f101eaf29b892ffc153b
onielrosario <onielrosario@pursuit.org>
2018-11-15 19:48:03 -0500

    final touches to do

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -6,15 +6,33 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum Square {
+    case x
+    case o
+    case empty
+    
+}
 
 class TicTacToeBrain {
     var playerTurn = Turn.playerOne
     
-    
-    enum Turn: String {
-        case playerOne = "X"
-        case playerTwo = "O"
+    enum Turn: String, CaseIterable {
+        case playerOne
+        case playerTwo
+        
+        func image() -> UIImage {
+            var buttonImage: UIImage!
+            switch self {
+            case .playerOne:
+                buttonImage = UIImage(named:"X")
+            default:
+                buttonImage = UIImage(named: "O")
+            }
+            
+            return buttonImage
+        }
         mutating func toggle() {
             switch self {
             case .playerOne: self = .playerTwo
@@ -23,25 +41,82 @@ class TicTacToeBrain {
         }
     }
     
-    func updateBrain(x: Int, y: Int){
-        playerTurn.toggle()
+    func updateBrain(row: Int, col: Int) {
         let square: Square
         switch playerTurn {
         case .playerOne: square = .x
         case .playerTwo: square = .o
         }
+        gameboard[row][col] = square
+        print(gameboard)
         
+        playerTurn.toggle()
     }
     
-    enum Square {
-        case x
-        case o
-        case empty
+    func checkForVictory() -> GameStatus {
+       
         
+        //diagonal wins//
+        var diagonal1 = [Square]()
+        for i in 0..<gameboard.count {
+            diagonal1.append(gameboard[i][i])
+        }
+        
+        if diagonal1 == [.x,.x,.x] {
+            return .playerOneVictory
+        } else if diagonal1 == [.o,.o,.o] {
+            return .playerTwoVictory
+        }
+        
+        var diagonal2 = [Square]()
+        for i in 0..<gameboard.count {
+            diagonal2.append(gameboard[i][gameboard.count - 1 - i])
+        }
+        if diagonal2 == [.x,.x,.x] {
+            return .playerOneVictory
+        } else if diagonal2 == [.o,.o,.o] {
+            return .playerTwoVictory
+        }
+        
+        
+        //row wins
+        for row in gameboard {
+            if row == [.x, .x, .x] {
+                return .playerOneVictory
+            } else if row == [.o,.o,.o] {
+                return .playerTwoVictory
+            }
+        }
+        
+        
+        //column wins
+        for columnIndex in 0..<gameboard[0].count {
+            var column = [Square]()
+            for rowIndex in 0..<gameboard.count {
+                column.append(gameboard[rowIndex][columnIndex])
+                if column == [.x,.x,.x] {
+                    return .playerOneVictory
+                } else  if column == [.o,.o,.o]{
+                    return .playerTwoVictory
+                }
+            }
+        }
+        return .inProgress
     }
+    
+    
+    
+    enum GameStatus {
+        case playerOneVictory, playerTwoVictory, tie, inProgress
+    }
+    
+    
     
     var gameboard =  [[Square]](repeating: [Square](repeating: .empty, count: 3), count: 3)
     
     
     
+    
+    
 }
+
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    
+    @IBOutlet var AllButtons: [GameButton]!
+    
     
     var gameBrain: TicTacToeBrain = TicTacToeBrain()
     
@@ -24,20 +28,49 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     }
+    //update brain checks who player it is
     
+    @IBOutlet weak var gameResult: UILabel!
     @IBAction func buttonPressed(_ sender: GameButton) {
         let turn = gameBrain.playerTurn
-        
-        sender.setTitle(turn.rawValue, for: .normal)
-        
-        gameBrain.updateBrain(x: sender.row, y: sender.col)
-        
-        print(sender)
-       
+        sender.setImage(turn.image(), for: .normal)
+        gameBrain.updateBrain(row: sender.row, col: sender.col)
+        print(gameBrain.checkForVictory())
+        switch gameBrain.checkForVictory() {
+        case .playerOneVictory:
+            gameResult.text = "Player One Wins"
+            AllButtons.forEach { (button) in
+                button.isEnabled = false
+            }
+        case .playerTwoVictory:
+            gameResult.text = "Player Two Wins"
+            AllButtons.forEach { (button) in
+                button.isEnabled = false
+            }
+        case .tie:
+            gameResult.text = "No Winner"
+            AllButtons.forEach { (button) in
+                button.isEnabled = false
+            }
+        case .inProgress:
+            print("default")
+        }
+        print(gameBrain)
         sender.isEnabled = false
         ///sender.backgroundColor = .black
     }
     
+    @IBAction func resetButton(_ sender: UIButton) {
+   gameBrain.gameboard = [[Square]](repeating: [Square](repeating: .empty, count: 3), count: 3)
+        let turn = gameBrain.playerTurn
+        //sender.setImage(turn.image(), for: .normal)
+        AllButtons.forEach{$0.isEnabled = true}
+        AllButtons.forEach { (button) in
+            button.setImage(UIImage(), for: .normal)
+            gameResult.text = "Click Anywhere to Start"
+        }
+    
+    }
     
 
 
